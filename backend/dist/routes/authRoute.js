2c021fa45fd1394437f579806bcef1427951df08
AnilaBenny <135572616+AnilaBenny@users.noreply.github.com>
2024-12-30 16:43:54 +0530

    update package.json

--- backend/dist/routes/authRoute.js
+++ backend/dist/routes/authRoute.js
@@ -4,10 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const employeeController_1 = require("../controllers/employeeController");
-const employeeRoute = express_1.default.Router();
-employeeRoute.post('/create', employeeController_1.createEmployee);
-employeeRoute.get('/all', employeeController_1.getAllEmployees);
-employeeRoute.put('/edit', employeeController_1.editEmployee);
-employeeRoute.delete('/delete/:employeeId', employeeController_1.deleteEmployee);
-exports.default = employeeRoute;
+const inventoryController_1 = require("../controllers/inventoryController");
+const inventoryRoute = express_1.default.Router();
+inventoryRoute.post('/items', inventoryController_1.createItem);
+inventoryRoute.get('/items', inventoryController_1.getAllItems);
+inventoryRoute.get('/items/:id', inventoryController_1.getItemById);
+inventoryRoute.put('/items/:id', inventoryController_1.updateItem);
+inventoryRoute.delete('/items/:id', inventoryController_1.deleteItem);
+exports.default = inventoryRoute;
--- backend/src/server.js
+++ backend/src/server.js
@@ -13,11 +13,9 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
-<<<<<<< HEAD
-    origin: ['http://localhost:5173', 'https://ems-beta-seven.vercel.app'],
-=======
-    origin: ['http://localhost:5173', 'https://ems-beta-seven.vercel.app/'],
->>>>>>> 5afab44142ee194537d5b6449620855f43059c5c
+
+    origin: ['http://localhost:5173', 'https://inventory-management-system-mu-tan.vercel.app'],
+
     credentials: true,
 }));
 app.use('/', authRoute_1.default);
